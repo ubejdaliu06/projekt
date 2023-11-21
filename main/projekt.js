d1610b9d65586173f8427b8a0fee578d4cda5a94
ubejd12232 <ua68003@ubt-uni.net>
2023-11-21 13:48:54 +0100

    Update projekt.js

--- main/projekt.js
+++ main/projekt.js
@@ -2,5 +2,4 @@
 console.log('Not for Sale for Minors - Products sold on this site may contain nicotine which is a highly addictive substance.! ')
 
 
-
-
+alert('Not for Sale for Minors - Products sold on this site may contain nicotine which is a highly addictive substance.! ')
