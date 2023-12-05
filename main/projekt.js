bce8429b8f9fec324fafb80c870e13c328e8cffc
ubejd12232 <ua68003@ubt-uni.net>
2023-12-05 14:09:17 +0100

    ubejfd

--- main/projekt.js
+++ main/projekt.js
@@ -3,3 +3,37 @@ console.log('Not for Sale for Minors - Products sold on this site may contain ni
 
 
 alert('Not for Sale for Minors - Products sold on this site may contain nicotine which is a highly addictive substance.! ')
+
+var i=0;
+var imgArray=[
+  "u1-removebg-preview.png",
+  "u2-removebg-preview.png",
+  "u3-removebg-preview.png",
+  "u4-removebg-preview.png",
+  "u5-removebg-preview.png",
+  "u6-removebg-preview.png",
+
+];
+function ndrroImg(){
+document.getElementById('fotografit').src =imgArray[i];
+ if(i<imgArray.length-1){
+  i++;
+ }else{
+  i--;
+ }
+//  setTimeout("ndrroImg()", 30000)
+}
+ document.body.addEventListener('load',ndrroImg());
+ function ktheImg(){
+  document.getElementById('fotografit').src =imgArray[i];
+   if(i<imgArray.length-1){
+    i--;
+   }else{
+    i++;
+   }
+  //  setTimeout("ndrroImg()", 30000)
+  }
+   document.body.addEventListener('load',ktheImg());
+  
+
+
